84679ceece535b05c50e3e9e9e109d6b379127d8
Mohammed Rehan Jaweed <151328966+Rehan-Jaweed@users.noreply.github.com>
2024-01-09 21:09:29 +0530

    Add files via upload

---
+++ Learnathon/day-2/Area.py
@@ -0,0 +1,10 @@
+# Taking input from the user
+length = float(input("Enter the length of the rectangle in cm : "))
+width = float(input("Enter the width of the rectangle in cm : "))
+
+# Calculating are of the rectangle
+area = length * width
+
+# Displaying results
+print("\nResults :")
+print("Area of the rectangle is : {:.2f}".format(area),"cm square units.")
