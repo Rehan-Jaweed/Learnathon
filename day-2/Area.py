aca7ab885c50629d65f61dc978862d4c29be0a7e
Mohammed Rehan Jaweed <151328966+Rehan-Jaweed@users.noreply.github.com>
2024-01-10 19:07:11 +0530

    Update Area.py

--- day-2/Area.py
+++ day-2/Area.py
@@ -1,3 +1,5 @@
+#this program prints area of the rectangle
+
 # Taking input from the user
 length = float(input("Enter the length of the rectangle in cm : "))
 width = float(input("Enter the width of the rectangle in cm : "))
