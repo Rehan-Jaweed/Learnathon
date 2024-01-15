062a5b47a4c9000cdb5aabba8b4b43a0b29fa53f
Mohammed Rehan Jaweed <151328966+Rehan-Jaweed@users.noreply.github.com>
2024-01-16 01:56:20 +0530

    Update Fahrenheit-Celsius.py

--- Day - 06 : Coding_Challenge  - 01/Fahrenheit-Celsius.py
+++ Day - 06 : Coding_Challenge  - 01/Fahrenheit-Celsius.py
@@ -4,4 +4,5 @@ fahrenheit = float(input("Enter temperature in celsius : "))
 
 # Calculating Celsius
 celsius = (fahrenheit - 32) / 1.8
+
 print('%0.1f degree Fahrenheit is equal to %0.1f degree Celsius.'%(fahrenheit,celsius))
